56d4137134f1956c2ef04fb999482812d117e047
irnbru-dev <darina.irnbru@gmail.com>
2019-08-26 01:10:18 +0300

    layout finished for 99% - adaptive done

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+$(".promo-owl").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    autoplay: true
+});
+
+$(".gallery-owl").owlCarousel({
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 30000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        992: {
+            items: 3
+        }
+    }
+});
