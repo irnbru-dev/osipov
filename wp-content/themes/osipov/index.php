8dac24d71489fa24c36d869e07f182b13e49da15
Bornyakova <darina.bornyakova@itglobal.com>
2019-10-31 10:30:15 +0300

    add ya metrika

--- wp-content/themes/osipov/index.php
+++ wp-content/themes/osipov/index.php
@@ -314,6 +314,30 @@
 <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 <script src="/wp-content/themes/osipov/js/script.min.js"></script>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(56009290, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        ecommerce: "dataLayer"
+    });
+</script>
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/56009290" style="position:absolute; left:-9999px;" alt=""/></div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php wp_footer(); ?>
 </body>
 </html>
